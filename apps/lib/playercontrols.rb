58cf8ce7db422b9e2795ceed8034193368c5f849
TanaTanoi <tana.tanoi@gmail.com>
2015-12-18 09:58:47 +1300

    Added configurations for xbox controllers (requires drivers for osx)

--- apps/lib/playercontrols.rb
+++ apps/lib/playercontrols.rb
@@ -1,5 +1,6 @@
 module PlayerControls
-  PLAYER1 = { :left => Gosu::KbLeft, :right => Gosu::KbRight, :up => Gosu::KbUp, :down => Gosu::KbDown }
-  PLAYER2 = { :left => Gosu::KbA, :right => Gosu::KbD, :up => Gosu::KbW, :down => Gosu::KbS }
+  PLAYER1 = { left: Gosu::KbLeft, right: Gosu::KbRight, up: Gosu::KbUp, down: Gosu::KbDown }
+  PLAYER2 = { left: Gosu::KbA, right: Gosu::KbD, up: Gosu::KbW, down: Gosu::KbS }
+  PLAYER3 = { left: 273, right: 274, up: 275, down: 276 }
+  PLAYER4 = { left: 279, right: 278, up: 280, down: 277 }
 end
-
